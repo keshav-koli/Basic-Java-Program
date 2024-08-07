fda1498eff557a63ca46e1ca8592679ba3f31207
keshav-koli <keshavkoli5588@gmail.com>
2024-08-07 09:34:22 +0530

    Added Prime Number.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -2,12 +2,12 @@ public class PrimeNumber {
     public static void main(String arg[]) {
         int n = 26;
         int divisble=0;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 2; i <= n/2; i++) {
             if (n % i == 0) {
                 divisble++;
             }
         }
-        if(divisble==2){
+        if(divisble==1){
             System.out.println("Prime Number");
         }
         else{
