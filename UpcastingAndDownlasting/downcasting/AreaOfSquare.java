6329e6fa6bc7a033571aae81a02390ea834cd902
keshav-koli <keshavkoli5588@gmail.com>
2024-09-23 23:38:44 +0530

    changes AreaOfSquare

---
+++ UpcastingAndDownlasting/downcasting/AreaOfSquare.java
@@ -0,0 +1,9 @@
+package UpcastingAndDownlasting.downcasting;
+
+public class AreaOfSquare extends Shape{
+    int length;
+    public void area_of_square(int a){
+        this.length=a;
+        System.out.println("Area of Square is "+this.length*this.length);
+    }
+}
