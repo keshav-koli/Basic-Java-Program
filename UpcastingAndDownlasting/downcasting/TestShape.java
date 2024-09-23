0857a77cff806aaf3bf1309f03c4a571b830cc08
keshav-koli <keshavkoli5588@gmail.com>
2024-09-23 23:48:50 +0530

    changes TestShape.java

--- UpcastingAndDownlasting/downcasting/TestShape.java
+++ UpcastingAndDownlasting/downcasting/TestShape.java
@@ -2,6 +2,11 @@ package UpcastingAndDownlasting.downcasting;
 
 public class TestShape {
     public static void main(String[] args) {
-        
+        Shape s1 = new AreaOfRectangle();
+        // s1.shape("Rectangle");
+        // AreaOfRectangle A1=(AreaOfRectangle)s1;
+        // A1.area_of_rectangle(4, 8);
+        // AreaOfSquare A2=(AreaOfSquare)s1;
+        // A2.area_of_square(8);
     }
 }
