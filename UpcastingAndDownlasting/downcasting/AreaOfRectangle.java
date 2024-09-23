a849b748743463feb415d6470a1584c4c313356c
keshav-koli <keshavkoli5588@gmail.com>
2024-09-23 23:33:58 +0530

    changes AreaOfRectangle.java

--- UpcastingAndDownlasting/downcasting/AreaOfRectangle.java
+++ UpcastingAndDownlasting/downcasting/AreaOfRectangle.java
@@ -1,5 +1,12 @@
 package UpcastingAndDownlasting.downcasting;
 
-public class AreaOfRectangle {
-    
+public class AreaOfRectangle extends Shape{
+    int length;
+    int breadth;
+
+    public void area_of_rectangle(int a,int b){
+        this.length=a;
+        this.breadth=b;
+        System.out.println("Area of Rectangle is "+this.length*this.breadth);
+    }
 }
