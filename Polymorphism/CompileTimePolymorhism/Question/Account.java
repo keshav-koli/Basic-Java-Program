7666aca36f6f9932b2dbc0bf2d2afa51fd9cf3a0
keshav-koli <keshavkoli5588@gmail.com>
2024-09-27 21:21:27 +0530

    added Question folder and Account.java

---
+++ Polymorphism/CompileTimePolymorhism/Question/Account.java
@@ -0,0 +1,24 @@
+package Polymorphism.CompileTimePolymorhism.Question;
+
+public class Account {
+    private int accountNumber;
+    private int accountHolder;
+    private int balance;
+
+    public void  set_Account(int accountHolder,int accountNumber,int balance) {
+        this.accountHolder=accountHolder;
+        this.accountNumber=accountNumber;
+        this.balance=balance;
+    }
+    public int get_holder(){
+        return accountHolder;
+    }
+    public int get_number(){
+        return accountNumber;
+    }
+    public int get_balance(){
+        return balance;
+    }
+
+
+}
