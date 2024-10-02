36dbe414c023a1493dcb59724ba3054c23b2f560
keshav-koli <keshavkoli5588@gmail.com>
2024-10-03 00:11:30 +0530

    changes in Market.java

--- Interface/Market.java
+++ Interface/Market.java
@@ -1,18 +1,14 @@
 package Interface;
-
 public interface Market {
     double manipulate();
-
     void sale();
-
 }
 
 class User {
     public void purchase() {
         System.out.println("Item Purchased Successfully");
     }
-
-    // double demand=1;
+    double demand=1;
     double price = 1000;
 
 }
@@ -22,31 +18,28 @@ class Seller extends User implements Market {
     double no_of_user;
     double no_of_items;
 
-    public Seller(double no_of_user, double no_of_items) {
+    public void set_demand(double no_of_user, double no_of_items) {
         this.no_of_user = no_of_user;
         this.no_of_items = no_of_items;
     }
 
     public double manipulate() {
-        double demand = no_of_user / no_of_items;
-        if (demand > 1) {
-            super.price = demand * super.price;
-        } else if (demand < 1) {
-            super.price = demand * super.price;
-        }
+        super.demand = no_of_user / no_of_items;
+        super.price = super.price * super.demand;
         return super.price;
     }
 
     public void sale() {
-        double demand = no_of_user / no_of_items;
-        if (demand < 1) {
-            int sale = (int) (super.price * demand);
-            System.out.println("Sale available of " + sale +"% off");
-            System.out.println("Price of item is " + (int)(super.price));
-        } else if(demand>=1){
-            System.out.println("Price of item is " + (int)(super.price));
-            System.out.println(demand + " item sold");
+       super.demand = no_of_user / no_of_items;
+        if (super.demand < 1) {
+            int discount = (int) ((1 - super.demand) * 100); 
+            System.out.println("Sale available of " + discount +"% off");
+            System.out.println("Price of item is Rs " + (int)(super.price)+"/-");
+            purchase();
+        } else if(super.demand>=1){
+            System.out.println("Price of item is Rs " + (int)(super.price)+"/-");
             System.out.println("No Sale available");
+            purchase();
         }
     }
 
--- Interface/TestMarket.java
+++ Interface/TestMarket.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class TestMarket {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        Seller se=new Seller(1,10);
-            // System.out.println("Enter number of user");
-            // double user=sc.nextDouble();
-            // System.out.println("Enter number of items");
-            // double items=sc.nextDouble();
-            // se.no_of_items=items;
-            // se.no_of_user=user;
+        Seller se=new Seller();
+            System.out.println("Enter number of user");
+            double user=sc.nextDouble();
+            System.out.println("Enter number of items");
+            double items=sc.nextDouble();
+            se.set_demand(user, items);
             se.manipulate();
             se.sale();
             // System.out.println(se.demand);
