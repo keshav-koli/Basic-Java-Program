e76bf54ffc7fdce6fe1e9559fb774e3bbb95bb7c
keshav-koli <keshavkoli5588@gmail.com>
2024-10-02 22:30:15 +0530

    added TestDriver.java

---
+++ Interface/TestDriver.java
@@ -0,0 +1,10 @@
+package Interface;
+
+public class TestDriver {
+    public static void main(String[] args) {
+        TV t=new TV();
+        t.turnOn();
+        t.turnOff();
+        t.work();
+    }
+}
