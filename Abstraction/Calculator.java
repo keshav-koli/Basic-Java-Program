b05d5bba8405beaee3cfd76f70570b4b0efea22c
keshav-koli <keshavkoli5588@gmail.com>
2024-10-04 13:18:26 +0530

    added Calculator.java

---
+++ Abstraction/Calculator.java
@@ -0,0 +1,25 @@
+package Abstraction;
+
+public abstract class Calculator{
+    abstract void add(int a, int b);
+    abstract void subtract(int a, int b);
+    abstract void multiply(int a, int b);
+    abstract void divide(int a, int b);
+}
+
+class Functionality extends Calculator{
+    public void add (int a,int b){
+        System.out.println("Addition is " +a+b );
+    } 
+    public void subtract (int a,int b){
+        System.out.println("subtraction is " +a+b );
+    } 
+    public void multiply (int a,int b){
+        System.out.println("multiply is " +a+b );
+    } 
+    public void divide (int a,int b){
+        System.out.println("divide is " +a+b );
+        
+    }
+}
+
