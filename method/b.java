f4581412b42986f5e6b8c51e5e1c4f9de01e3d82
keshav-koli <keshavkoli5588@gmail.com>
2024-10-08 07:58:20 +0530

    changes in b.java

--- method/b.java
+++ method/b.java
@@ -1,11 +1,26 @@
 package method;
 public class b {
-    static float a;
+    static void m1(){
+        System.out.println("m1");
+    }
+    static float a=10.5f;
+    static void m2(){
+        m1();
+        System.out.println(a);
+        System.out.println(b.a);
+        b.m1();
+        System.out.println("m2");
+    }
     
 }
 class c{
     static int a;
         public static void main(String arg[]){
+            b.m1();
+            b.m2();
+            b.a=10;
+            System.out.println(a);
         System.out.println(b.a);
+        System.out.println(c.a);
         }
 }
